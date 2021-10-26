60848aac1a1b6fd5bcabc2dfd75bb0575f5d8e24
byungchul_yun <byungchul_yun@tmax.co.kr>
2021-10-26 21:35:01 +0900

    [Edit] 온도 변환 Page JavaScript Logic 추가 1. index.html의 Page List에 Temperature Converter 추가 2. 온도 변환 Page에서 input과 output입력 창의 fontsize를 줄임 3. JavaScript Logic을 이용하여 온도 변환이 정상적으로 동작하도록 함 	selector를 변경하거나, input 이나 output의 값이 변화하는 순간에 	onChange를 이용하여 값을 계산하는 Logic을 추가

--- IntroductionOfWeb/javascript/temperatureConverter.js
+++ IntroductionOfWeb/javascript/temperatureConverter.js
@@ -1,53 +1,145 @@
 //Celsius To Farenheit 연산 함수
 function calculateCtoF(celsius) {
-    var result = 0;
-
-    result = (celsius * 9 / 5) + 32;
-
-    console.log('Calculated Farenheit' + ' = ' + result);
-
-    document.getElementById("CtoFResultParagraph").innerHTML = result;
+    return (celsius * 9 / 5) + 32;
 }
 
-//Celsius To Farenheit 연산 함수
+//Celsius To Kelvin 연산 함수
+function calculateCtoK(celsius) {
+    return celsius + 273.15;
+}
+
+//Farenheit To Celsius 연산 함수
 function calculateFtoC(farenheit) {
+    return (farenheit - 32) * (5 / 9);
+}
+
+//Kelvin To Celsius 연산 함수
+function calculateKtoC(kelvin) {
+    return kelvin - 273.15;
+}
+
+//계산된 결과값을 입력한 input의 반대에 넣어주는 과정
+function showResult(output, result) {
+    console.log("Converted Result = " + result);
+    output.value = result;
+}
+
+var input = document.getElementById("inputTemperature");
+var inputSelector = document.getElementById("inputSelector");
+var output = document.getElementById("outputTemperature");
+var outputSelector = document.getElementById("outputSelector");
+
+input.onchange = function () { inputConvert(input, inputSelector, outputSelector, output) };
+outputSelector.onchange = function () { inputConvert(input, inputSelector, outputSelector, output) };
+output.onchange = function () { inputConvert(output, outputSelector, inputSelector, input) };
+inputSelector.onchange = function () { inputConvert(output, outputSelector, inputSelector, input) };
+
+function inputConvert(input, inputSelector, outputSelector, output) {
+    var inputValue = Number(input.value);
+    var outputValue = Number(output.value);
+    var inputSelectorIndex = inputSelector.selectedIndex;
+    var outputSelectorIndex = outputSelector.selectedIndex;
+
     var result = 0;
 
-    result = (farenheit - 32) * (5 / 9);
+    console.log("input Value = " + inputValue);
+    console.log("output Value = " + outputValue);
+    console.log("input Value Selector Index = " + inputSelectorIndex);
+    console.log("output Value Selector Index = " + outputSelectorIndex);
 
-    console.log('Calculated Celsius' + ' = ' + result);
-
-    document.getElementById("FtoCResultParagraph").innerHTML = result;
-}
-
-//입력한 값이 숫자인지 반환하는 함수
-function isValidInput(number) {
-    return !isNaN(Number(number))
-}
-
-var inputButton = document.getElementById("temperatureInputButton");
-
-inputButton.onclick = function () {
-    //온도 입력받기
-    var isInputValid = false;
-    var temperatureInput = 0;
-
-    while (!isInputValid) {
-        temperatureInput = prompt("변환할 온도를 입력해주세요.\n단위는 생략해주세요.", 0);
-        console.log("Input Temperature Value = " + temperatureInput);
-        //prompt 취소 Button check
-        if (temperatureInput === null) {
-            return;
-        }
-
-        isInputValid = isValidInput(temperatureInput);
-        if (isInputValid === false) {
-            alert("입력하신 값이 잘못되었습니다.\n옳바른 값을 입력해 주세요.");
-        }
+    switch (inputSelectorIndex) {
+        case 0:
+            //From Celsius
+            console.log("From Celsius");
+            switch (outputSelectorIndex) {
+                case 0:
+                    //To Celsius
+                    console.log("To Celsius");
+                    console.log("Input Type And Output Type Same");
+                    result = inputValue;
+                    break;
+                case 1:
+                    //To Farenheit
+                    console.log("To Farenheit");
+                    result = calculateCtoF(inputValue);
+                    break;
+                case 2:
+                    //To Kelvin
+                    console.log("To Kelvin");
+                    result = calculateCtoK(inputValue);
+                    break;
+            }
+            break;
+        case 1:
+            //From Farenheit
+            console.log("From Farenheit");
+            switch (outputSelectorIndex) {
+                case 0:
+                    //To Celsius
+                    console.log("To Celsius");
+                    result = calculateFtoC(inputValue);
+                    break;
+                case 1:
+                    //To Farenheit
+                    console.log("To Farenheit");
+                    console.log("Input Type And Output Type Same");
+                    result = inputValue;
+                    break;
+                case 2:
+                    //To Kelvin
+                    console.log("To Kelvin");
+                    result = calculateCtoK(calculateFtoC(inputValue));
+                    break;
+            }
+            break;
+        case 2:
+            //From Kelvin
+            console.log("From Kelvin");
+            switch (outputSelectorIndex) {
+                case 0:
+                    //To Celsius
+                    console.log("To Celsius");
+                    result = calculateKtoC(inputValue);
+                    break;
+                case 1:
+                    //To Farenheit
+                    console.log("To Farenheit");
+                    result = calculateCtoF(calculateKtoC(inputValue));
+                    break;
+                case 2:
+                    //To Kelvin
+                    console.log("To Kelvin");
+                    console.log("Input Type And Output Type Same");
+                    result = inputValue;
+                    break;
+            }
+            break;
     }
 
-    calculateInputToC(temperatureInput);
-    calculateInputToF(temperatureInput);
-    calculateInputToK(temperatureInput);
+    showResult(output, result);
+}
 
-};
+// inputButton.onclick = function () {
+//     //온도 입력받기
+//     var isInputValid = false;
+//     var temperatureInput = 0;
+
+//     while (!isInputValid) {
+//         temperatureInput = prompt("변환할 온도를 입력해주세요.\n단위는 생략해주세요.", 0);
+//         console.log("Input Temperature Value = " + temperatureInput);
+//         //prompt 취소 Button check
+//         if (temperatureInput === null) {
+//             return;
+//         }
+
+//         isInputValid = isValidInput(temperatureInput);
+//         if (isInputValid === false) {
+//             alert("입력하신 값이 잘못되었습니다.\n옳바른 값을 입력해 주세요.");
+//         }
+//     }
+
+//     calculateInputToC(temperatureInput);
+//     calculateInputToF(temperatureInput);
+//     calculateInputToK(temperatureInput);
+
+// };
