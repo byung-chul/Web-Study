82ebd99cf891cdcb7ddfaf4c4b5d62f0f4b03c2e
byungchul_yun <byungchul_yun@tmax.co.kr>
2021-11-04 22:27:55 +0900

    [New] 입력한 Text를 대문자나 소문자 중 이루어진 문장을 확인하는 기능 추가 입력받은 Text가 대문자로만 이루어진 문장인지, 소문자로만 이루어진 문장인지, 일반 문장인지를 확인하는 기능을 추가함 입력받은 text와 text의 toUpperCase()가 같으면 대문자로만 이루어진 문자 입력받은 text와 text의 toLowerCase()가 같으면 소문자로만 이루어진 문자 이외에는 보통문장으로 처리함

--- IntroductionOfWeb/javascript/calculator.js
+++ IntroductionOfWeb/javascript/calculator.js
@@ -21,6 +21,7 @@ function calculate(firstInput, secondInput, operator) {
 
     console.log(first + ' ' + operator + ' ' + second + ' = ' + result);
 
+    document.getElementById("inputParagraph").innerHTML = first + ' ' + operator + ' ' + second + ' = ' + result;
     document.getElementById("resultParagraph").innerHTML = result;
 }
 
@@ -38,6 +39,18 @@ function isEven(input) {
     let inputNumber = Number(input);
 
     return inputNumber % 2 === 0 ? true : false;
+}
+
+//입력한 Input이 숫자인지 아닌지 반환하는 함수
+function typeText(input) {
+
+    if (input === input.toUpperCase()) {
+        return 1;
+    } else if (input === input.toLowerCase()) {
+        return 2;
+    } else {
+        return 3;
+    }
 }
 
 //입력한 연산 종류가 적정한지 반환하는 함수
@@ -127,11 +140,43 @@ oddOrEvenButton.onclick = function () {
 
     console.log("Input is = " + input);
 
+    let inputParagraph = document.getElementById("inputOddOrEven")
     let resultParagraph = document.getElementById("resultOddOrEven")
+
+    inputParagraph.innerHTML = "입력하신 수는 " + input + "입니다.";
+
     if (isEven(input)) {
         resultParagraph.innerHTML = "짝수입니다.";
     } else {
         resultParagraph.innerHTML = "홀수입니다.";
     }
 
+}
+
+let upperOrLowerButton = document.getElementById("upperOrLowerButton");
+
+upperOrLowerButton.onclick = function () {
+    //숫자 입력 받기
+    var input = prompt("확인 할 문장을 입력해 주세요.");
+    //prompt 취소 Button check
+    if (input === null) {
+        return;
+    }
+
+    console.log("Input is = " + input);
+
+    let inputParagraph = document.getElementById("inputUpperOrLower")
+    let resultParagraph = document.getElementById("resultUpperOrLower")
+
+    let type = typeText(input);
+
+    inputParagraph.innerHTML = "입력하신 text는 " + input + "입니다.";
+
+    if (type === 1) {
+        resultParagraph.innerHTML = "대문자로 이루어진 문장입니다.";
+    } else if (type === 2) {
+        resultParagraph.innerHTML = "소문자로 이루어진 문장입니다.";
+    } else if (type === 3) {
+        resultParagraph.innerHTML = "보통 문장입니다.";
+    }
 }
